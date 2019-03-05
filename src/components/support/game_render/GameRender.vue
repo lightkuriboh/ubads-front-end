53d03493fff82b974124fdfbccef49202e9b89b8
kuribohkute <kuribohwithwing@gmail.com>
2019-03-05 12:21:56 +0700

    Add game_render files

--- src/components/support/game_render/GameRender.vue
+++ src/components/support/game_render/GameRender.vue
@@ -1,14 +1,19 @@
 <template>
     <div>
-      <div id="container"></div>
       <el-button @click="test()" type="danger">
         Start Game
       </el-button>
+      <div id="container"></div>
     </div>
 </template>
 
 <script>
-import constant from 'game_render/utils/Constant'
+import constant from 'game_render/js/utils/Constant'
+import Phaser from 'game_render/js/phaser'
+import LoadingScene from 'game_render/js/scenes/LoadingScene'
+import GameScene from 'game_render/js/scenes/GameScene'
+import StartScene from 'game_render/js/scenes/StartScene'
+import dataGame from 'game_render/data'
 
 export default {
   name: 'GameRender',
@@ -17,18 +22,26 @@ export default {
     }
   },
   created () {
-    // let scripts = [
-    //   'https://cdn.jsdelivr.net/npm/phaser@3.11.0/dist/phaser.js'
-    // ]
-    // for (let srcScript of scripts) {
-    //   let script = document.createElement('script')
-    //   script.setAttribute('src', srcScript)
-    //   document.head.appendChild(script)
-    // }
   },
   methods: {
     test: function () {
-      console.log(constant)
+      console.log(dataGame)
+      let config = {
+        type: Phaser.AUTO,
+        width: constant.MAP_WIDTH * constant.SCALE,
+        height: constant.MAP_HEIGHT * constant.SCALE,
+        parent: 'container',
+        scene: [LoadingScene, StartScene, GameScene]
+        // scene: {
+        //   init: LoadingScene,
+        //   preload: StartScene,
+        //   create: GameScene
+        // }
+      }
+      let game = new Phaser.Game(config)
+      game.scene.stop('StartScene')
+      game.scene.stop('GameScene')
+      game.scene.run('StartScene', dataGame)
     }
   }
 }
